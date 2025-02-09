5eaeef653d8fdab01d720310c43f78d76c21e2eb
Divyanshu Sharma <160989334+Divyanshu3181@users.noreply.github.com>
2025-02-09 12:47:45 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,3 @@
-//export const BASE_URL =
- // location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
 export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:3000/api"
-    : "https://devspark-backend.up.railway.app"; 
+ location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
